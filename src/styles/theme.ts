447639c74b60082de198663e7fe88c904ded9319
Calenzo <calenzodevs@gmail.com>
2024-10-25 16:21:02 -0300

    feat: Configurado o estilo do projeto

---
+++ src/styles/theme.ts
@@ -0,0 +1,16 @@
+export const theme = {
+  dark: {
+    colors: {
+      primary: "#7CDF51",
+      secondary: "#272727",
+      text: "#FFFFFF",
+    },
+  },
+  white: {
+    colors: {
+      primary: "#1E6200",
+      secondary: "#FFFFFF",
+      text: "#272727",
+    },
+  },
+};
